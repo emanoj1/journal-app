506a6aaaace138eb2be57069dd7285c60ad04251
Manoj Kumar <37259245+emanoj1@users.noreply.github.com>
2024-05-16 21:03:49 +1000

    fix: update entry functionality

--- src/components/EntryForm.jsx
+++ src/components/EntryForm.jsx
@@ -10,21 +10,25 @@ import { useState } from "react"
 
 export default function EntryForm(props){
 
-	let [localTitle, setLocalTitle] = useState(props.entryData.title);
-	let [localAuthor, setLocalAuthor] = useState(props.entryData.author);
-	let [localContent, setLocalContent] = useState(props.entryData.content);
+	let [localTitle, setLocalTitle] = useState((props.entryData && props.entryData.title) || "Default Title");
+	let [localAuthor, setLocalAuthor] = useState(props.entryData ? props.entryData.author : "Default Author");
+	let [localContent, setLocalContent] = useState(props.entryData?.content || "Hello world! Write a journal entry!");
 	// may do date later!
 
 	return(
 		<div>
 			<label htmlFor="entryTitle">Title:</label>
-			<input type="text" name="entryTitle" class="entryTitle" value={localTitle} onChange={(event) => setLocalTitle(event.target.value) }      />
+			<input type="text" name="entryTitle" className="entryTitle" value={localTitle} onChange={(event) => setLocalTitle(event.target.value) }      />
 
 			<label htmlFor="entryAuthor">Author:</label>
-			<input type="text" name="entryAuthor" class="entryAuthor" value={localAuthor} onChange={(event) => setLocalAuthor(event.target.value) }      />
+			<input type="text" name="entryAuthor" className="entryAuthor" value={localAuthor} onChange={(event) => setLocalAuthor(event.target.value) }      />
 
 			<label htmlFor="entryContent">Content:</label>
-			<input type="text" name="entryContent" class="entryContent" value={localContent} onChange={(event) => setLocalContent(event.target.value) }      />
+			<input type="text" name="entryContent" className="entryContent" value={localContent} onChange={(event) => setLocalContent(event.target.value) }      />
+
+			<button onClick={() => props.addEntry(localTitle, localAuthor, localContent, Date.now(), props.entryData.id)}>
+				Submit entry
+			</button>
 		</div>
 	)
 }
